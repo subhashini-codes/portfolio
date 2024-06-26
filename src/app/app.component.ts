9c126d4b5a374a087ca54510f7e5eed26c2a1df3
sathish <me.sathish.git@gmail.com>
2024-06-26 22:46:48 +0530

    add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent, NavComponent } from './components';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -2,3 +2,6 @@ export * from './about/index';
 export * from './contact/index';
 export * from './home/index';
 export * from './page-not-found/page-not-found.component';
+
+// nav and footer components
+export * from './shared/index';
