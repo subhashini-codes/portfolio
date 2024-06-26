3584d65a2223d6511030af713c306b3b0989301a
sathish <me.sathish.git@gmail.com>
2024-06-26 21:45:31 +0530

    add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {
+  AboutComponent,
+  ContactComponent,
+  HomeComponent,
+  PageNotFoundComponent,
+} from './components';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
---
+++ src/app/components/index.ts
@@ -0,0 +1,4 @@
+export * from './about/index';
+export * from './contact/index';
+export * from './home/index';
+export * from './page-not-found/page-not-found.component';
