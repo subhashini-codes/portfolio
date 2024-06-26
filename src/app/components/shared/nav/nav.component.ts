28710c428816df8c8270128184a2ab449377730c
sathish <me.sathish.git@gmail.com>
2024-06-26 23:28:34 +0530

    add nave bar

--- src/app/components/shared/nav/nav.component.ts
+++ src/app/components/shared/nav/nav.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.scss'
+  styleUrl: './nav.component.scss',
 })
 export class NavComponent {
-
+  toggleHidden() {
+    document.getElementById('menu-toggle')?.classList.toggle('hidden');
+  }
 }
